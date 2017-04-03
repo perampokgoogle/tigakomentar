250950bb0b0ef709e2306d3248867437cf55427f
perampokgoogle <localhost367@gmail.com>
2017-04-03 09:35:15 +0700

    Update tigacomments.js

--- tigacomments.js
+++ tigacomments.js
@@ -24,7 +24,7 @@ var disqus_url = disqus_blogger_current_url;
     }
 })();
 
-!function(e,n,t){var o,c=e.getElementsByTagName(n)[0];e.getElementById(t)||(o=e.createElement(n),o.id=t,o.src="//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
+!function(e,n,t){var o,c=e.getElementsByTagName(n)[0];e.getElementById(t)||(o=e.createElement(n),o.id=t,o.src="//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
 
     var divs = ["disqus-box", "blogger-box", "facebook-box"];
     var visibleDivId = null;
